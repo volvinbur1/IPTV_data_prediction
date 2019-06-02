0815e88ea3ea07b49c7b36baf813fa7f43ce65c5
DarkNeon135 <47756626+DarkNeon135@users.noreply.github.com>
2019-06-03 02:53:51 +0300

    Add files via upload

--- AlgorithmSupportVectorMachine.cs
+++ AlgorithmSupportVectorMachine.cs
@@ -1,7 +1,36 @@
-﻿namespace IPTV_Qality_Prediction
+﻿using Accord.MachineLearning;
+using Accord.MachineLearning.VectorMachines.Learning;
+using Accord.Math.Optimization.Losses;
+using Accord.Statistics.Kernels;
+using com.sun.corba.se.spi.orbutil.fsm;
+
+
+namespace IPTV_Qality_Prediction
+
 {
-    public static partial class Algorithm
+    public partial class Algorithm
     {
+        public void SupportVectorMachineInOu(double[][] input, double[] output)
+        {
+
+        }
         
+        public void SupportVectorMachineLearning(double[][] input, int [] output)
+        {
+            var teacher = new MulticlassSupportVectorLearning<Linear>();
+            {
+                System.Func<object, LinearDualCoordinateDescent> Learner = (p) => new LinearDualCoordinateDescent()
+                {
+                    Loss = Loss.L2
+                };
+
+            }
+            teacher.ParallelOptions.MaxDegreeOfParallelism = 1;
+            var machine = teacher.Learn(input, output);
+            int[] predicted = machine.Decide(input);
+            double error = new ZeroOneLoss(output).Loss(predicted);
+
+
+        }
     }
 }
