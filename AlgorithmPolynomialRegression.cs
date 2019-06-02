aad3d57674a5251375815d2e586bd91040339b23
Volodymyr Burdeinyi <44869508+volvinbur1@users.noreply.github.com>
2019-06-02 22:08:19 +0300

    Add files via upload

--- AlgorithmPolynomialRegression.cs
+++ AlgorithmPolynomialRegression.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Accord.Statistics.Models.Regression.Linear;
 
 namespace IPTV_Qality_Prediction
 {
-    public static partial class Algorithm
+    public partial class Algorithm
     {
+        private double[][] LearningData;
+        private double[] coefOfFunction;
+
+        public Algorithm(double[][] array)
+        {
+            LearningData = array;
+        }
+
+        public void PolynomialRegressionGetFuncOneIndependentVariable(int degree, double[] independentVariables, double[] dependentVariables)
+        {
+            PolynomialRegression objRegression =
+                PolynomialRegression.FromData(degree, independentVariables, dependentVariables);
+
+            coefOfFunction = new double[objRegression.Weights.Length + 1];
+
+            coefOfFunction[0] = objRegression.Intercept;
+
+            int index = objRegression.Weights.Length - 1;
+
+            for (int i = 1; i <= objRegression.Weights.Length; i++)
+            {
+                coefOfFunction[i] = objRegression.Weights[index];
+                index--;
+            }
+        }
     }
 }
