9cc078b2a60af347d08d3a2c536aa3fc35acd792
Volodymyr Burdeinyi <44869508+volvinbur1@users.noreply.github.com>
2019-06-02 19:53:04 +0300

    Add files via upload

---
+++ WorkWithLearningData.cs
@@ -0,0 +1,10 @@
+﻿namespace IPTV_Qality_Prediction
+{
+    public static class WorkWithLearningData
+    {
+        public static double[][] ReadFromFile(string path)
+        {
+            return new double[0][];
+        }
+    }
+}
