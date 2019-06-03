5ae10641f173de19a72dc4f57ad8cfc682ca919d
Volodymyr Burdeinyi <44869508+volvinbur1@users.noreply.github.com>
2019-06-03 03:58:55 +0300

    Add files via upload

--- AlgorithmPolynomialRegression.cs
+++ AlgorithmPolynomialRegression.cs
@@ -12,7 +12,7 @@ namespace IPTV_Qality_Prediction
             LearningData = array;
         }
 
-        public void PolynomialRegressionGetFuncOneIndependentVariable(int degree, double[] independentVariables, double[] dependentVariables)
+        private void PRLearningOneIndVar(int degree, double[] independentVariables, double[] dependentVariables)
         {
             PolynomialRegression objRegression =
                 PolynomialRegression.FromData(degree, independentVariables, dependentVariables);
@@ -28,6 +28,20 @@ namespace IPTV_Qality_Prediction
                 coefOfFunction[i] = objRegression.Weights[index];
                 index--;
             }
+        }
+
+        public void PolynomialRegressionLearning()//int colunmIndex)
+        {
+            int n = LearningData.Length;
+            double[] dependentVariables = new double[n], independentVariables = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                dependentVariables[i] = LearningData[i][3];
+                independentVariables[i] = LearningData[i][2];
+            }
+
+            PRLearningOneIndVar(3, independentVariables, dependentVariables);
         }
     }
 }
