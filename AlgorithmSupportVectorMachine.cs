f7fc5b913d3ddcbaf574425af40ae5416f48d68a
DarkNeon135 <47756626+DarkNeon135@users.noreply.github.com>
2019-06-06 01:43:43 +0300

    Add files via upload

--- AlgorithmSupportVectorMachine.cs
+++ AlgorithmSupportVectorMachine.cs
@@ -1,38 +1,97 @@
-﻿using Accord.MachineLearning;
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Accord.MachineLearning.VectorMachines.Learning;
 using Accord.Math.Optimization.Losses;
 using Accord.Statistics.Kernels;
-using System;
+using sun.security.util;
 
 namespace IPTV_Qality_Prediction
 
 {
     public partial class Algorithm
     {
+        public double[][] NormalizedInputData;
+
+        private double Maximum(double[] valueDoubles)
+        {
+            double max = double.MinValue;
+
+            foreach (var digit in valueDoubles)
+                if (digit > max)
+                    max = digit;
+
+            return max;
+        }
+
+        private double Minimum(double[] valueDoubles)
+        {
+            double min = double.MaxValue;
+
+            foreach (var digit in valueDoubles)
+                if (digit < min)
+                    min = digit;
+
+            return min;
+        }
+
+        public void InputDataNormalization()
+        {
+            int n = LearningData.Length;
+            NormalizedInputData = new double[n][];
+            
+            for (int i = 0; i < 3; i++)
+            {
+                double[] temp = new double[n];
+
+                for (int j = 0; j < n; j++)
+                {
+                    temp[j] = LearningData[j][i];
+                    NormalizedInputData[j] = new double[3];
+                }
+
+                double minDependentValue = Minimum(temp);
+                double maxDependentValue = Maximum(temp);
+
+                for (int j = 0; j < n; j++)
+                {
+                    NormalizedInputData[j][i] = (LearningData[j][i] - minDependentValue) /
+                                                (maxDependentValue - minDependentValue);
+                }
+            }
+        }
+
         public void SupportVectorMachineLearning()
         {
             int n = LearningData.Length;
-            int[] input = new int[n];
-            double[][] output = new double[n][];
-
+            int[] output = new int[n];
+            double[][] input = new double[n][];
+            InputDataNormalization();
             for (int i = 0; i < n; i++)
             {
-                input[i] = Convert.ToInt32(LearningData[i][3]);
+                output[i] = Convert.ToInt32(LearningData[i][3]);
 
-                output[i] = new double[3];
-                output[i][0] = LearningData[i][0];
-                output[i][1] = LearningData[i][1];
-                output[i][2] = LearningData[i][2];
+                input[i] = new double[3];
+                input[i][0] = LearningData[i][0];
+                input[i][1] = LearningData[i][1];
+                input[i][2] = LearningData[i][2];
             }
 
-            SVMLearning(output,input);
+
+            SVMLearning(input, output);
         }
-        
+       
+
+
+      
         private void SVMLearning(double[][] input, int[] output)
         {
             var teacher = new MulticlassSupportVectorLearning<Linear>();
             {
-                System.Func<object, LinearDualCoordinateDescent> Learner = (p) => new LinearDualCoordinateDescent()
+                LinearDualCoordinateDescent Learner(object p) => new LinearDualCoordinateDescent()
                 {
                     Loss = Loss.L2
                 };
